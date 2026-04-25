f7a516152adb6e1eb41f7285f909a65a0de148a3
Jugnu Dahiya <jugnudahiya624@gmail.com>
2026-04-25 20:04:37 +0530

    fix firebase admin for production

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccount.json");
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
