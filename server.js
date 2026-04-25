e7b8abf2e59f58691fa296afb4a77391a2110496
Jugnu Dahiya <jugnudahiya624@gmail.com>
2026-04-25 18:17:12 +0530

    fix port for railway

--- server.js
+++ server.js
@@ -27,4 +27,5 @@ app.use((err, _req, res, _next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
